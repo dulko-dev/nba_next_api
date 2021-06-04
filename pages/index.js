0d1c9f6f1cf80101b80b1c4a23382a736cdc7d83
Kamil Duliniec <123dulko@wp.pl>
2021-06-04 16:43:49 +0200

    styled title and add pic for home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+
 export default function Home() {
-  return null;
+  return (
+    <div className={styles.home}>
+      <Image src="/lebron.png" alt="player lebron" width={200} height={250} />
+      <div className={styles.title}>
+        <p>T</p>
+        <p>E</p>
+        <p>A</p>
+        <p>M</p>
+        <p>S</p>
+      </div>
+      <Image
+        src="/westbrook.png"
+        alt="player westbrook"
+        width={200}
+        height={250}
+      />
+    </div>
+  );
 }
