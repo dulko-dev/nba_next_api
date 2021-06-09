5e2067ffc3b2526e739ef7d8ba9567d7497e8913
Kamil Duliniec <123dulko@wp.pl>
2021-06-09 14:54:14 +0200

    styled contact form, validated and start style validation

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import { send } from "emailjs-com";
@@ -11,10 +11,36 @@ function Contact() {
     from_name: "",
     message: "",
     reply_to: "",
+    from_number: "",
   });
+
+  let inputField;
+
+  useEffect(() => {
+    inputField = document.getElementById("replyTo");
+    if (toSend.reply_to.length > 0) {
+      inputField.style.border = "1px solid rgb(212, 212, 212)";
+    }
+  }, [toSend.reply_to]);
+
+  const [error, setError] = useState(false);
 
   const onSubmit = (e) => {
     e.preventDefault();
+    inputField = document.getElementById("replyTo");
+
+    if (toSend.reply_to.length === 0) {
+      inputField.style.border = "1px solid red";
+    }
+
+    if (
+      toSend.from_name.trim().length < 3 ||
+      toSend.message.length === 0 ||
+      toSend.reply_to.length === 0
+    ) {
+      setError(true);
+      return;
+    }
 
     send(
       "service_vbte9e8",
@@ -28,6 +54,13 @@ function Contact() {
       .catch((err) => {
         console.log("FAILED", err);
       });
+    setToSend({
+      from_name: "",
+      message: "",
+      reply_to: "",
+      from_number: "",
+    });
+    setError(false);
   };
 
   const handleChange = (e) => {
@@ -54,32 +87,78 @@ function Contact() {
             </a>
           </Link>
         </div>
+
         <form onSubmit={onSubmit}>
-          <label htmlFor="fromName" className={styles.label}>Your Name</label>
-          <input
-            type="text"
-            id="fromName"
-            name="from_name"
-            value={toSend.from_name}
-            onChange={handleChange}
-          />
-          <label htmlFor="replyTo" className={styles.label}>Your email</label>
-          <input
-            type="text"
-            id="replyTo"
-            name="reply_to"
-            value={toSend.reply_to}
-            onChange={handleChange}
-          />
-          <label htmlFor="message" className={styles.label}>Message</label>
-          <textarea
-            type="text"
-            id="message"
-            name="message"
-            value={toSend.message}
-            onChange={handleChange}
-          />
-          <button type="submit" className={styles.button}>Button</button>
+          {error && (
+            <div
+              style={{
+                textAlign: "center",
+                color: "red",
+                textTransform: "uppercase",
+                padding: "10px 0",
+              }}
+            >
+              Please fill empty fields
+            </div>
+          )}
+          <fieldset className={styles.fieldset}>
+            <legend className={styles.legend}>
+              or You can send me an message
+            </legend>
+            <div className={styles.row}>
+              <label htmlFor="fromName" className={styles.label}>
+                Name
+              </label>
+              <input
+                type="text"
+                id="fromName"
+                name="from_name"
+                value={toSend.from_name}
+                onChange={handleChange}
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.row}>
+              <label htmlFor="replyTo" className={styles.label}>
+                Email
+              </label>
+              <input
+                type="email"
+                id="replyTo"
+                name="reply_to"
+                value={toSend.reply_to}
+                onChange={handleChange}
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.row}>
+              <label htmlFor="fromNumber" className={styles.label}>
+                Number<span style={{ fontSize: "0.5em" }}>(optional)</span>
+              </label>
+              <input
+                type="number"
+                id="fromNumber"
+                name="from_number"
+                value={toSend.from_number}
+                onChange={handleChange}
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.row}>
+              <textarea
+                placeholder="Place for the message"
+                type="text"
+                id="message"
+                name="message"
+                value={toSend.message}
+                onChange={handleChange}
+                className={styles.textarea}
+              />
+            </div>
+            <button type="submit" className={styles.button}>
+              Send
+            </button>
+          </fieldset>
         </form>
       </div>
     </>
